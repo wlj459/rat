45b3fd2bf1ac292762975887a124ede4ae1d16a2
wlj459 <wlj459@gmail.com>
2016-05-01 13:11:27 +0800

    进程打印也完成了

--- client.py
+++ client.py
@@ -2,4 +2,9 @@
 import rpyc
 c = rpyc.connect("127.0.0.1", 11111)
 print c.root.get_file_list("/")
+print "^^^^^^^^^^^^^^^^^^^^^^"
+a = c.root.get_process_list()
+print "^^^^^^^^^^^^^^^^^^^^^^"
+for i in a:
+        print i
 c.close()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 import os
+import psutil
 from rpyc import Service
 from rpyc.utils.server import ThreadedServer
 
@@ -7,6 +8,11 @@ from rpyc.utils.server import ThreadedServer
 class RemoteCallScript(Service):
     def exposed_get_file_list(self, path):
         return os.listdir(path)
+
+    def exposed_get_process_list(self):
+        p = psutil.process_iter()
+        return list(p)
+
 
 s = ThreadedServer(RemoteCallScript, port=11111, auto_register=False)
 s.start()
