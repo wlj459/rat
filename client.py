52d29a2898e6ba0c9f9102f19b2f3b5151794b30
wlj459 <wlj459@gmail.com>
2016-05-01 15:03:04 +0800

    愉快的水完了

--- client.py
+++ client.py
@@ -1,10 +1,17 @@
 # -*- coding:utf-8 -*-
 import rpyc
 c = rpyc.connect("127.0.0.1", 11111)
-print c.root.get_file_list("/")
+a = c.root.listdir("/")
+for i in a:
+    print i
 print "^^^^^^^^^^^^^^^^^^^^^^"
-a = c.root.get_process_list()
-print "^^^^^^^^^^^^^^^^^^^^^^"
+a = c.root.process_list()
 for i in a:
         print i
+print "^^^^^^^^^^^^^^^^^^^^^^"
+# a = c.root.move('/Users/libai/code/rat/test', "/Users/libai/code/rat/yooo")
+print "^^^^^^^^^^^^^^^^^^^^^^"
+# a = c.root.rmdir('/Users/libai/code/rat/yooo')
+a = c.root.shell("ls")
+print a
 c.close()
--- server.py
+++ server.py
@@ -1,18 +1,34 @@
 # -*- coding:utf-8 -*-
 import os
 import psutil
+import shutil
+import commands
 from rpyc import Service
 from rpyc.utils.server import ThreadedServer
 
 
 class RemoteCallScript(Service):
-    def exposed_get_file_list(self, path):
+    def exposed_listdir(self, path):
         return os.listdir(path)
 
-    def exposed_get_process_list(self):
+    def exposed_rmdir(self, path):
+            return shutil.rmtree(path)
+
+    def exposed_move(self, oldpos, newpos):
+        if not os.path.exists(oldpos):
+            return False
+
+        if not os.path.exists(newpos):
+            os.makedirs(newpos)
+        return shutil.move(oldpos, newpos)
+
+    def exposed_process_list(self):
         p = psutil.process_iter()
         return list(p)
 
+    def exposed_shell(self, cmd):
+        a = commands.getoutput(cmd)
+        return a
 
 s = ThreadedServer(RemoteCallScript, port=11111, auto_register=False)
 s.start()
